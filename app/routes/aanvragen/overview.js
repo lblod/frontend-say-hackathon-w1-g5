4b5b11c66e3564dd9e20051a5f06c42930cd0fed
felix <ruiz.felixruiz@gmail.com>
2024-09-13 13:55:20 +0200

    fixes

--- app/routes/aanvragen/overview.js
+++ app/routes/aanvragen/overview.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class AanvragenOverviewRoute extends Route {
   async model() {
-    const response = await fetch('https://aanvraag.hackathon-5.s.redhost.be/aanvraag')
+    const response = await fetch('/aanvraag')
     return await response.json()
   }
 }
