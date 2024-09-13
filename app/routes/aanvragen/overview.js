3093d16f12efd3eb9af2308291b093e022e8ce01
Niels Steenbergen <niels.steenbergen@triply.cc>
2024-09-13 12:28:07 +0200

    First stuff
    Co-authored-by: kikef98 <114907819+kikef98@users.noreply.github.com>

--- app/routes/aanvragen/overview.js
+++ app/routes/aanvragen/overview.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
 
-export default class AanvragenOverviewRoute extends Route {}
+export default class AanvragenOverviewRoute extends Route {
+  async model() {
+    // const response = await fetch('https://aanvraag.hackathon-5.s.redhost.be/aanvraag')
+    const response = await fetch('http://localhost/aanvraag')
+    return await response.json()
+  }
+}
