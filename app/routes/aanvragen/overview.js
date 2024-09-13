c6c5c7cbfcd431776373b9f4924d898a9fe80982
Kike Franssen <114907819+kikef98@users.noreply.github.com>
2024-09-13 12:52:12 +0200

    tabelstructuur en connectie

--- app/routes/aanvragen/overview.js
+++ app/routes/aanvragen/overview.js
@@ -2,8 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class AanvragenOverviewRoute extends Route {
   async model() {
-    // const response = await fetch('https://aanvraag.hackathon-5.s.redhost.be/aanvraag')
-    const response = await fetch('http://localhost/aanvraag')
+    const response = await fetch('https://aanvraag.hackathon-5.s.redhost.be/aanvraag')
     return await response.json()
   }
 }
