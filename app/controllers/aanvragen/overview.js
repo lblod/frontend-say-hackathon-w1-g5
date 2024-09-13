f586d1ec0159ceae740afd3bf3197565bf54e6d1
Kike Franssen <114907819+kikef98@users.noreply.github.com>
2024-09-13 12:37:36 +0200

    spelfout overicht

--- app/controllers/aanvragen/overview.js
+++ app/controllers/aanvragen/overview.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default class AanvragenOverviewController extends Controller {
-  title = 'Overicht aanvragen toelatingen beschermd onroerend erfgoed';
+  title = 'Overzicht aanvragen toelatingen beschermd onroerend erfgoed';
   @service() router;
 
   get hasActiveChildRoute() {
