253545ffb4d7d530a8f23ebfbf3ea6e645f2788f
Niels Steenbergen <niels.steenbergen@triply.cc>
2024-09-13 15:01:42 +0200

    Add sample besluiten data to frontend
    This won't be used but I forgot to commit it

--- app/routes/aanvragen/overview.js
+++ app/routes/aanvragen/overview.js
@@ -2,7 +2,8 @@ import Route from '@ember/routing/route';
 
 export default class AanvragenOverviewRoute extends Route {
   async model() {
-    const response = await fetch('https://aanvraag.hackathon-5.s.redhost.be/aanvraag')
-    return await response.json()
+    const aanvragen = await fetch('https://aanvraag.hackathon-5.s.redhost.be/aanvraag')
+    const besluiten = await fetch('https://aanvraag.hackathon-5.s.redhost.be/besluiten')
+    return {aanvraag: await aanvragen.json(), besluit: await besluiten.json()}
   }
 }
